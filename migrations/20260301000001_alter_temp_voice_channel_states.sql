649965dd51baf3e00f08d881f838297e531741e8
Vadim SkinonikS.# <skinnoniks@gmail.com>
2026-03-01 23:41:26 +0200

    update migration goose statements for consistency

--- migrations/20260301000001_alter_temp_voice_channel_states.sql
+++ migrations/20260301000001_alter_temp_voice_channel_states.sql
@@ -1,5 +1,9 @@
 -- +goose Up
+-- +goose StatementBegin
 ALTER TABLE temp_voice_channel_states DROP COLUMN member_count;
+-- +goose StatementEnd
 
 -- +goose Down
+-- +goose StatementBegin
 ALTER TABLE temp_voice_channel_states ADD COLUMN member_count INTEGER NOT NULL DEFAULT 0;
+-- +goose StatementEnd
